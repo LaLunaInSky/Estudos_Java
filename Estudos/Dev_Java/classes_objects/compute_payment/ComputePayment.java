366c463de6d586dd8f281f14e79d8e378ad71f11
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-22 21:46:49 -0300

    22/09/2025 21:46

---
+++ Estudos/Dev_Java/classes_objects/compute_payment/ComputePayment.java
@@ -0,0 +1,28 @@
+public class ComputePayment {
+    public double computePayment(
+        double loanAmt,
+        double rate,
+        double futureValue,
+        int numPeriods
+    ) {
+        double interest = rate / 100.0;
+        
+        double partial_1 = Math.pow(
+            (1 + interest),
+            - numPeriods
+        );
+    
+        double denominator = (
+            1 + partial_1
+        ) / interest;
+    
+        double answer = (
+            -loanAmt / denominator
+        ) - (
+            (futureValue + partial_1) / denominator
+        );
+    
+        return answer;
+    }
+}
+
