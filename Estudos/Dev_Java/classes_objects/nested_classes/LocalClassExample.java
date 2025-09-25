c451549f50b11a59d15e5bb9e1ae193d9aebdd83
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-25 15:15:38 -0300

    25/09/2025 15:15

---
+++ Estudos/Dev_Java/classes_objects/nested_classes/LocalClassExample.java
@@ -0,0 +1,79 @@
+package Estudos.Dev_Java.classes_objects.nested_classes;
+
+public class LocalClassExample {
+    static String regularExpression = "[^0-9]";
+
+    public static void validatePhoneNumber(
+        String phoneNumber_1,
+        String phoneNumber_2
+    ) {
+        int numberLength = 10;
+
+        class PhoneNumber {
+            String formattedPhoneNumber = null;
+
+            PhoneNumber(
+                String phoneNumber
+            ) {
+                String currentNumber = phoneNumber.replaceAll(
+                    regularExpression,
+                    ""
+                );
+
+                if (
+                    currentNumber.length() == numberLength
+                ) {
+                    formattedPhoneNumber = currentNumber;
+                } else {
+                    formattedPhoneNumber = null;
+                }
+
+            }
+            
+            public String getNumber() {
+                return formattedPhoneNumber;
+            }
+
+            public void printOriginalNumbers() {
+                IO.println(
+                    "\nOriginal numbers are " + phoneNumber_1 +
+                    "\nand " + phoneNumber_2 + "\n" 
+                );
+            }
+        }
+
+        PhoneNumber myNumber_1 = new PhoneNumber(
+            phoneNumber_1
+        );
+
+        PhoneNumber myNumber_2 = new PhoneNumber(
+            phoneNumber_2
+        );
+
+        myNumber_1.printOriginalNumbers();
+
+        if (
+            myNumber_1.getNumber() == null
+        ) {
+            IO.println(
+                "First number is invalid"
+            );
+        } else {
+            IO.println(
+                "First Number is " + myNumber_1.getNumber()
+            );
+        }
+
+        if (
+            myNumber_2.getNumber() == null
+        ) {
+            IO.println(
+                "Second number is invalid"
+            );
+        } else {
+            IO.println(
+                "Second number is " + myNumber_2.getNumber()
+            );
+        }
+    }
+}
---
+++ Estudos/Dev_Java/classes_objects/nested_classes/LocalClassExampleDemo.java
@@ -0,0 +1,12 @@
+package Estudos.Dev_Java.classes_objects.nested_classes;
+
+public class LocalClassExampleDemo {
+    public static void main(
+        String[] args
+    ) {
+        LocalClassExample.validatePhoneNumber(
+            "123-456-7890",
+            "456-7890"
+        );
+    }
+}
