39bc136128233c1339ee492836065a3c54de98dd
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-25 15:40:02 -0300

    25/09/2025 15:40

---
+++ Estudos/Dev_Java/classes_objects/nested_classes/HelloWorldAnonymousClassesDemo.java
@@ -0,0 +1,11 @@
+package Estudos.Dev_Java.classes_objects.nested_classes;
+
+public class HelloWorldAnonymousClassesDemo {
+    public static void main(
+        String... args
+    ) {
+        HelloWorldAnonymousClasses myApp = new HelloWorldAnonymousClasses();
+
+        myApp.sayHello();
+    }
+}
