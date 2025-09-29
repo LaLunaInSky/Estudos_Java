900a7ce602ee1240b14a21d7fd4974fc2e4b7319
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-29 17:44:27 -0300

    29/09/2025 17:44

---
+++ Estudos/Dev_Java/numbers_and_strings/PrimitiveTypesNumbers.java
@@ -0,0 +1,16 @@
+package Estudos.Dev_Java.numbers_and_strings;
+
+public class PrimitiveTypesNumbers {
+    public static void main(
+        String... args
+    ) {
+        int i = 500;
+        float gpa = 3.65f;
+        byte mask = 0x7f;
+
+        System.out.format(
+            "i = %d \ngpa = %f \nmask = %d%n",
+            i, gpa, mask
+        );
+    }
+}
