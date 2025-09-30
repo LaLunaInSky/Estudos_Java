5824942eed18b3282ceec04cc2d1d55622237d66
LaLunaInSky <moonaflordapele@gmail.com>
2025-09-30 12:11:17 -0300

    30/09/2025 12:11

---
+++ Estudos/Dev_Java/numbers_and_strings/DecimalFormatDemo.java
@@ -0,0 +1,46 @@
+import java.text.*;
+
+public class DecimalFormatDemo {
+    public static void customFormat(
+        String pattern,
+        double value
+    ) {
+        DecimalFormat myFormatter = new DecimalFormat(
+            pattern
+        );
+
+        String output = myFormatter.format(
+            value
+        );
+
+        IO.println(
+            value + " " +
+            pattern + " " + 
+            output
+        );
+    }
+
+    public static void main(
+        String... args 
+    ) {
+        customFormat(
+            "###,###.###",
+            123456.789    
+        );
+
+        customFormat(
+            "###.##",
+            123456.789
+        );
+
+        customFormat(
+            "000000.000",
+            123.78
+        );
+
+        customFormat(
+            "$###,###.###",
+            12345.67
+        );
+    }
+}
