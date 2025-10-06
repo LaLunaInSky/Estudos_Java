8ca8d3b1958c6831e209d63e4cde0c0fc80c4f38
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-06 12:26:48 -0300

    06/10/2025 12:26

---
+++ Estudos/Dev_Java/inheritance/inheritance/Animal.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class Animal {
+    public static void testClassMethod() {
+        IO.println(
+            "The static method in Animal"
+        );
+    }
+
+    public void testInstanceMethod() {
+        IO.println(
+            "The instance method in Animal"
+        );
+    }
+}
---
+++ Estudos/Dev_Java/inheritance/inheritance/Cat.java
@@ -0,0 +1,27 @@
+package inheritance;
+
+public class Cat extends Animal {
+    public static void testClassMethod() {
+        IO.println(
+            "The static method in Cat"
+        );
+    }
+
+    public void testInstanceMethod() {
+        IO.println(
+            "The instance method in Cat"
+        );
+    }
+
+    public static void main(
+        String... args
+    ) {
+        Cat myCat = new Cat();
+
+        Animal myAnimal = myCat;
+
+        Animal.testClassMethod();
+
+        myAnimal.testInstanceMethod();
+    }
+}
