ebe5ede07e3affac1a77992a289c738ada9a5458
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-07 08:31:33 -0300

    07/10/2025 08:31

--- Estudos/Dev_Java/inheritance/abstract_methods_and_classes/GraphicObject.java
+++ Estudos/Dev_Java/inheritance/abstract_methods_and_classes/GraphicObject.java
@@ -1,5 +1,15 @@
 package abstract_methods_and_classes;
 
-public abstract class GraphicObject {
+abstract class GraphicObject {
+    int x, y;
+
+    void moveTo(
+        int newX,
+        int newY
+    ) {
+        // Implementation here
+    }
+
     abstract void draw();
+    abstract void resize();
 }
---
+++ Estudos/Dev_Java/inheritance/abstract_methods_and_classes/Rectangle.java
@@ -0,0 +1,11 @@
+package abstract_methods_and_classes;
+
+public class Rectangle extends GraphicObject {
+    public void draw() {
+        // Implementation here
+    }
+
+    public void resize() {
+        // Implementation here
+    }
+}
