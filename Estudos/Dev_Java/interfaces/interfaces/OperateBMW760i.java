72e7fe7ae2d8f2de08d179c93aec1800acd82752
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-07 13:30:38 -0300

    07/10/2025 13:30

---
+++ Estudos/Dev_Java/interfaces/interfaces/OperateBMW760i.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+public class OperateBMW760i implements OperateCar {
+    public void signalTurn(
+            Direction direction,
+            boolean signalOn
+    ) {
+
+    };
+}
