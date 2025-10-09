8bf0ca3c3487dac2ca1f2e7e6ddf900092dad972
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-09 13:52:14 -0300

    09/10/2025 13:52

---
+++ Estudos/Dev_Java/generics/introducing_generics/Box.java
@@ -0,0 +1,15 @@
+package introducing_generics;
+
+public class Box<T> {
+    private T t;
+
+    public void set(
+        T t
+    ) {
+        this.t = t;
+    }
+
+    public T get() {
+        return t;
+    }
+}
--- Estudos/Dev_Java/generics/introducing_generics/Main.java
+++ Estudos/Dev_Java/generics/introducing_generics/Main.java
@@ -1,5 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
 void main() {
-    IO.println(
-        "Main"
-    );
+//    List list = new ArrayList();
+//
+//    list.add("hello");
+//
+//    String s = (String) list.get(0);
+
+    // Or generic
+
+    List<String> list = new ArrayList<String>();
+
+    list.add("hello");
+
+    String s = list.get(0);
 }
