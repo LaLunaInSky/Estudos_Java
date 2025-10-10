cd64e0a69ad94a00f42d2ee62c4bd2c26616c4ff
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-10 18:28:24 -0300

    10/10/2025 18:28

--- Estudos/Dev_Java/generics/introducing_generics/Main.java
+++ Estudos/Dev_Java/generics/introducing_generics/Main.java
@@ -1,3 +1,5 @@
+import introducing_generics.OrderedPair;
+
 import java.util.List;
 import java.util.ArrayList;
 
@@ -16,4 +18,27 @@ void main() {
     list.add("hello");
 
     String s = list.get(0);
+
+    //
+
+//    Pair<String, Integer> p_01 = new OrderedPair<String, Integer>(
+//        "Even",
+//        8
+//    );
+//
+//    Pair<String, String> p_02 = new OrderedPair<String, String>(
+//        "hello",
+//        "world"
+//    );
+
+    // Or
+    OrderedPair<String, Integer> p_01 = new OrderedPair<>(
+        "Even",
+        8
+    );
+
+    OrderedPair<String, String> p_02 = new OrderedPair<>(
+        "hello",
+        "world"
+    );
 }
