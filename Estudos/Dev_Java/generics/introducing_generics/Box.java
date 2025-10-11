e77f008154fca89216dade45ffd08f7f314d2e3b
LaLunaInSky <moonaflordapele@gmail.com>
2025-10-11 12:05:38 -0300

    11/10/2025 12:05

--- Estudos/Dev_Java/generics/introducing_generics/Box.java
+++ Estudos/Dev_Java/generics/introducing_generics/Box.java
@@ -12,4 +12,33 @@ public class Box<T> {
     public T get() {
         return t;
     }
+
+    public <U extends Number> void inspect(
+        U u
+    ) {
+        IO.println(
+            "\nT: " + t.getClass().getName() +
+            "\nU: " + u.getClass().getName() +
+            "\n"
+        );
+    }
+
+    void main() {
+        Box<Integer> integerBox = new Box<Integer>();
+
+        integerBox.set(
+            new Integer(
+                10
+            )
+        );
+
+        // Error
+//        integerBox.inspect(
+//            "some text"
+//        );
+
+        integerBox.inspect(
+            8
+        );
+    }
 }
